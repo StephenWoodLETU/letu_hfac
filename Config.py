e278fd67aeed13e7657b81ee4d8932d5598b42a9
Daniel Rothfus <danielrothfus@users.sourceforge.net>
2012-02-25 13:58:41 -0600

    Added configuration file and device tuple pattern
    Devices are now represented as a tuple with the com port/filename and
    baud rate.

---
+++ Config.py
@@ -0,0 +1,9 @@
+# All the configuration variables that are used by all the tests
+
+# Used only for the tests not directly testing power
+DEFAULT_POWER = .2
+
+# Com port/baud rate pairs for the input/output devices
+LOAD_DEVICE = ("com1", 57600)
+TUNE_DEVICE = ("com1", 57600)
+FREQ_DEVICE = ("com1", 57600)
--- LoadControl.py
+++ LoadControl.py
@@ -2,7 +2,10 @@
 
 class LoadControl:
 	
-	def __init__(self, comPort):
+	def __init__(self, device):
+		"""Open a specific device as a load controller.  Device is
+		a tuple with the first element being the com port on Windows or
+		file name on Linux and the second element is the baud rate."""
 		pass
 
 	def __del__(self):
