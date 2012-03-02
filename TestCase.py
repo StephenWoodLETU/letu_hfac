1863af237a5c68a7cc4b80dbc1c9e30d92588022
jimtahu <jimtahu@gmail.com>
2012-03-02 13:42:09 -0600

    Wrote a base class for TestCases.
    new file:   TestCase.py

---
+++ TestCase.py
@@ -0,0 +1,37 @@
+#provides a base class for the other testcases 
+
+import sys
+import time
+import Config
+from LoadControl import LoadControl
+from FreqControl import FrequencyControl
+from TuneControl import TunerControl
+
+class TestCase:
+	def __init__(self, name):
+		self.name=name
+		self.Load=LoadControl(Config.LOAD_DEVICE)
+		self.Freq=FrequencyControl(Config.FREQ_DEVICE)
+		self.Tune=TunerControl(Config.TUNE_DEVICE)
+	def runtests(self):
+		print "Would you like to run "+self.name+"?(Y/N)",
+		respons=raw_input()
+		if respons[0] != 'Y' and respons[0] != 'y':
+			return False
+		Started=time.time()
+		ret=self.test()
+		Finished=time.time()
+		if ret:
+			print self.name,"PASSED"
+		else:
+			print self.name,"FAILED"
+		print "test took",Finished-Started,"seconds"
+		return ret
+	def test(self):
+		print "Writing to screen"
+		return True
+
+if __name__ == '__main__':
+	test=TestCase("FakeTest")
+	test.runtests()
+
