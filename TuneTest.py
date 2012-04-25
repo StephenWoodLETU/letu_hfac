4bb2060a0acd65d1cd33674b78965441674b0d85
jimtahu <jimtahu@gmail.com>
2012-04-25 14:38:21 -0500

    Fixed frequency loop and removed load from output

--- TuneTest.py
+++ TuneTest.py
@@ -29,7 +29,7 @@ class TuneTest(TestCase):
             
         #open output
         results = csv.writer(file(Config.TuneTestoutput,"wb"))
-        results.writerow(["Frequency","Load (L, C, R)","VSWR","Status","Visual"])
+        results.writerow(["Frequency","VSWR","Status","Visual"])
         
         # Cycle through frequencies
         for line in tableJ:
@@ -38,75 +38,67 @@ class TuneTest(TestCase):
             roe = 0
             roeValues = None
             vswrFile = None
-                    
+            
+            frequency = float(line[Config.J_FREQ_COL])
             #set freq?
-            self.freq.setFrequency(float(line[Config.J_FREQ_COL]))
+            self.freq.setFrequency(frquency)
             for offset in range(-1,Config.J_LOADS_PER_VSR*Config.J_VSRS,3):        
                 # Set load
-                print("Items %s %s %s" %
-                  (line[Config.J_L_COL+offset],
-                           line[Config.J_C_COL+offset],
-                           line[Config.J_R_COL+offset]))
-                print("Setting load: L: %g C: %g R: %g" %
-                  (float(line[Config.J_L_COL+offset]),
-                           float(line[Config.J_C_COL+offset]),
-                           float(line[Config.J_R_COL+offset])))
-                self.loadSetter.setLCR(
-                float(line[Config.J_L_COL+offset]),
-                float(line[Config.J_C_COL+offset]),
-                float(line[Config.J_R_COL+offset]))
+                L = float(line[Config.J_L_COL+offset])
+                C = float(line[Config.J_C_COL+offset])
+                R = float(line[Config.J_R_COL+offset])
+                print("Setting load: L: %g C: %g R: %g" % (L,C,R))
+                self.loadSetter.setLCR(L,C,R)
                               
-                # Prompt user to run the network analyzer and save to location
-                while(vswrFile == None):                    
-                    print("Press enter to set the transmitter to recieve")
-                    self.wait()
-                    self.freq.setRx()
-                    print("Please set the network analyzer to  %s Hz"
-                     % line[Config.J_FREQ_COL])
-                    print("Please save the CSV data from the analyzer to %s"
-                     % Config.NET_RES_FILE)
-                    self.wait()
-                    try:
-                        vswrFile = file(Config.NET_RES_FILE, "r")
-                    except:
-                        print("Could not open input file") 
-                print "Does the graph look correct? :",
-                notes = raw_input()
-                print("Reconnect the coupler to the transmitter then press enter")
+            # Prompt user to run the network analyzer and save to location
+            while(vswrFile == None):                    
+                print("Press enter to set the transmitter to recieve")
                 self.wait()
-                self.freq.setTx()
-    
-                # Load the contents of the file
-                roeValues = [newLine for newLine in csv.reader(vswrFile)][Config.NET_RES_HEADER_ROWS:]
-                vswrFile.close()
-                vswrFile = None
-                # Calculate average roe 
-                roeValues.pop()
-                for roeLine in roeValues:
-                    try:
-                        roe += Utils.tooComplex(roeLine[Config.NET_RES_ROE_COL])
-                    except IndexError:
-                        print "Wiered input:",roeLine
-                roe /= len(roeValues)
-                
-                # Calculate VSWR and compare to predicted
-                calcVSWR = Utils.Vswr(roe)
-                
-                # Print results and set overall test to false if failed
-                if(curTestResult):
-                    print("Pass for frequency %s" % (line[Config.J_FREQ_COL]))
-                    results.writerow([float(line[Config.J_FREQ_COL]),
-                    (line[Config.J_L_COL],line[Config.J_C_COL],line[Config.J_R_COL]),
-                    calcVSWR,"Pass",notes]) 
-                    testResult = True
-                else:
-                    print("Fail for frequency %s" % (line[Config.J_FREQ_COL]))
-                    results.writerow([float(line[Config.J_FREQ_COL]),
-                    (line[Config.J_L_COL],line[Config.J_C_COL],line[Config.J_R_COL]),
-                    calcVSWR,"Fail",notes])
-                    testResult = False
-            if(not self.prompt("Continue?")):
-                return testResult
+                self.freq.setRx()
+                print("Please set the network analyzer to  %s Hz"
+                 % line[Config.J_FREQ_COL])
+                print("Please save the CSV data from the analyzer to %s"
+                 % Config.NET_RES_FILE)
+                self.wait()
+                try:
+                    vswrFile = file(Config.NET_RES_FILE, "r")
+                except:
+                    print("Could not open input file") 
+            print "Does the graph look correct? :",
+            notes = raw_input()
+            print("Reconnect the coupler to the transmitter then press enter")
+            self.wait()
+            self.freq.setTx()
+
+            # Load the contents of the file
+            roeValues = [newLine for newLine in csv.reader(vswrFile)][Config.NET_RES_HEADER_ROWS:]
+            vswrFile.close()
+            vswrFile = None
+            # Calculate average roe 
+            roeValues.pop()
+            for roeLine in roeValues:
+                try:
+                    roe += Utils.tooComplex(roeLine[Config.NET_RES_ROE_COL])
+                except IndexError:
+                    print "Wiered input:",roeLine
+            roe /= len(roeValues)
+            
+            # Calculate VSWR and compare to predicted
+            calcVSWR = Utils.Vswr(roe)
+            
+            # Print results and set overall test to false if failed
+            if(curTestResult):
+                print("Pass for frequency %s" % (frequency))
+                results.writerow([float(line[Config.J_FREQ_COL]),
+                calcVSWR,"Pass",notes]) 
+                testResult = True
+            else:
+                print("Fail for frequency %s" % (frequency))
+                results.writerow([float(line[Config.J_FREQ_COL]),
+                calcVSWR,"Fail",notes])
+                testResult = False
+        if(not self.prompt("Continue?")):
+            return testResult
                 
         return testResult
     
