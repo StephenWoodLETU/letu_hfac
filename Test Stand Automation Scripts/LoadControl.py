7b71ea97984f39774068a286b94098abe75340fa
Daniel Rothfus <danielrothfus@users.sourceforge.net>
2012-12-11 12:06:59 -0600

    Started I2C connection with load control arduino
    This is just the current progress (not working yet) on the connection
    between the Raspberry Pi that run the test scripts and the Arduino that
    sets loads via I2C.  The Arduino code is a test system and the changes
    will later be merged into the actual load control when it is useful. I
    also modified the interface a bit - by adding a null character at the
    end of command transition, but that may be unecessary and I will work to
    change it back if possible.

--- Test Stand Automation Scripts/LoadControl.py
+++ Test Stand Automation Scripts/LoadControl.py
@@ -1,47 +1,98 @@
-# This file communicates with the load Arduino.
-# The format for this is:
-#  setup by sending "L C R;"
-#  reset by sending "<char>"
+# This file communicates with the load Arduino over I2C
 
 import time
-import serial 
+import smbus
+
+# The default address for the arduino to be found at
+DEFAULT_ADDRESS = 0x10
+
+# The bounds for the resistance, inductance, and capacitance
+R_BOUNDS = (2, 1000)
+L_BOUNDS = (31, 8000)
+C_BOUNDS = (15, 18800)
+
+def _clamp(value, bounds):
+        """Return value within the boundaries given.  Bounds is a tuple with the
+        lower and upper limits."""
+        
+        return min(max(value, bounds[0]), bounds[1])
 
 class LoadControl:
         
-        def __init__(self, device):
-               """Open a specific device as a load controller.  Device is
-               a tuple with the first element being the com port on Windows or
-               file name on Linux and the second element is the baud rate."""
-               try:
-                     self.device = device
-                     self.comlink = serial.Serial(*device)
-               except:
-                     print "Could not open ",device
-                     raise
+        def __init__(self, arduinoAddress = DEFAULT_ADDRESS):
+                """Connect to an Arduino on a specific I2C address specified by
+                address.  This throws an exception if it cannot use I2C."""
+                
+                try:
+                    self.smbus = smbus.SMBus(0)
+                    self.address = arduinoAddress
+                except IOError:
+                    print "Could not open connection to load control arduino!"
+                    raise
         
-        def __del__(self):
-               self.comlink.close()
-
-        def setLCR(self, l, c, r):
-               # Format is L C R;
-               command = b"{0} {1} {2};".format(l,c,r)
-               self.comlink.write(command)
-               self.comlink.flush()
-
-        def reset(self):
-               # Format is <char>
-               self.comlink.write(b"#")
-               self.comlink.flush()
+        def setLoad(self, l, c, r):
+                """Set the load (inductance, capacitance, and resistance in that
+                order) and return the combined amount that could not be met.
+                The returned value is probably not very useful unless you are
+                checking that each load was met exactly, in which case it will
+                be 0."""
+                
+                totalRemainder = 0
+                
+                totalRemainder += self.setInductance(l)
+                totalRemainder += self.setCapacitance(c)
+                totalRemainder += self.setResistance(r)
+                
+                return totalRemainder
+               
+        def _getRemainder(self):
+                """Get the remainder of the capacitance, inductance, or
+                resistance that could not be fulfilled in the last command or -1
+                if there was an error reading from the device."""
+                
+                try:
+                        return self.smbus.read_byte(self.address)
+                except:
+                        return -1
+               
+        def _sendCommand(self, command, data):
+                """Send a low-level command to the device along with an integer
+                argument."""
+                
+                try:
+                        command = map(ord, str(command))
+                        argument = map(ord, str(data))
+                        for byte in command + argument + [0]:
+                                self.smbus.write_byte(self.address, byte)
+                except IOError:
+                        return -1
+               
+        def setResistance(self, r):
+                """Set the resistance (ohms) of the load and return the amount
+                that could not be met or -1 if the connection was lost."""
+                
+                self._sendCommand("SETR", _clamp(int(r), R_BOUNDS))
+                return self._getRemainder()
+                
+        def setInductance(self, l):
+                """Set the inductance (mH) of the load and return the amount
+                that could not be met or -1 if the connection was lost."""
+                
+                self._sendCommand("SETL", _clamp(int(l), L_BOUNDS))
+                return self._getRemainder()
+                
+        def setCapacitance(self, c):
+                """Set the capacitance (uF) of the load and return the amount
+                that could not be met or -1 if the connection was lost."""
+                
+                self._sendCommand("SETC", _clamp(int(c), C_BOUNDS))
+                return self._getRemainder()
 
 if __name__ == '__main__':
         print("Testing LoadControl!")
-        f = raw_input("Enter the device descriptor: ")
-        b = int(raw_input("Enter the baud rate: "))
-        load = LoadControl((f,b))
+        load = LoadControl()
         L = int(raw_input("Enter a load: "))
         C = int(raw_input("Enter a capacatence: "))
         R = int(raw_input("Enter a resistance: "))
-        load.setLCR(L,C,R)
-        time.sleep(5)
-        load.reset()
-
+        load.setLoad(L,C,R)
+        
--- Test Stand Load Control/LoadControl/LoadControl.ino
+++ Test Stand Load Control/LoadControl/LoadControl.ino
@@ -11,47 +11,19 @@
 // I2C Libraries:
 #include "Wire.h"
 
-// Pin Assignments:
-int const Res2_5  = 22;
-int const Res5    = 23; 
-int const Res12   = 24; 
-int const Res25   = 25;
-int const Res50   = 26;
-int const Res100  = 41;
-int const Res250  = 40;
-int const Res500  = 39;
-int const Res1000 = 38;
-
-int const Cap15    = 28;
-int const Cap33    = 29;
-int const Cap68    = 30;
-int const Cap120   = 31;
-int const Cap330   = 32;
-int const Cap680   = 47;
-int const Cap1000  = 46;
-int const Cap2200  = 45;
-int const Cap4700  = 44;
-int const Cap9400  = 43;
-int const Cap18800 = 42;
-
-int const Ind31_25m = 33;
-int const Ind62_5m  = 34;
-int const Ind125m   = 35;
-int const Ind250m   = 36;
-int const Ind500m   = 37;
-int const Ind1      = 52;
-int const Ind2      = 51;
-int const Ind4      = 50;
-int const Ind8      = 49;
-
 // Define I2C Commands
 #define SET_RES_COMMAND  "SETR"
 #define SET_IND_COMMAND  "SETL"
 #define SET_CAP_COMMAND  "SETC"
 #define CMD_LENGTH       4
+#define MAX_CMD_LENGTH   20
 
 // Define the slave address of the Arduino (Any number is fine... We'll use 16)
 #define SLAVE_ADDRESS 0x10
+
+// Buffer for receiving command into
+char command[MAX_CMD_LENGTH + 1];
+int commandIndex = 0;
 
 // Global variable to send back a response from the arduino to the RasPi
 // Note that a response of 0 indicates an exact match was made. Any other value
@@ -59,41 +31,18 @@ int const Ind8      = 49;
 int response = 1;
 
 void setup() {
-  pinMode(Res2_5, OUTPUT);
-  pinMode(Res5, OUTPUT);
-  pinMode(Res12, OUTPUT);
-  pinMode(Res25, OUTPUT);
-  pinMode(Res50, OUTPUT);
-  pinMode(Res250, OUTPUT);
-  pinMode(Res500, OUTPUT);
-  pinMode(Res1000, OUTPUT);
-  pinMode(Cap15, OUTPUT);
-  pinMode(Cap33, OUTPUT);
-  pinMode(Cap68, OUTPUT);
-  pinMode(Cap120, OUTPUT);
-  pinMode(Cap330, OUTPUT);
-  pinMode(Cap680, OUTPUT);
-  pinMode(Cap1000, OUTPUT);
-  pinMode(Cap2200, OUTPUT);
-  pinMode(Cap4700, OUTPUT);
-  pinMode(Cap9400, OUTPUT);
-  pinMode(Cap18800, OUTPUT);
-  pinMode(Ind31_25m, OUTPUT);
-  pinMode(Ind62_5m, OUTPUT);
-  pinMode(Ind125m, OUTPUT);
-  pinMode(Ind250m, OUTPUT);
-  pinMode(Ind500m, OUTPUT);
-  pinMode(Ind1, OUTPUT);
-  pinMode(Ind2, OUTPUT);
-  pinMode(Ind4, OUTPUT);
-  pinMode(Ind8, OUTPUT);
   
   // Initialize I2C as slave
   Wire.begin(SLAVE_ADDRESS);
   
+  // Debug serial begin
+  Serial.begin(9600);
+  
   // Define callbacks for I2C communication
   Wire.onReceive(receiveData);
   Wire.onRequest(sendData);
+  
+  Serial.println("Load controller started...");
 }
 
 void loop() {
@@ -103,30 +52,32 @@ void loop() {
 // callback for received data
 void receiveData(int byteCount) 
 {
-  // The command tupple contains both the operation to be taken and the value (operand) to set
-  String commandTupple = "";
-  String commandValue = "";
-  String commandOperation = "";
   
-  while(Wire.available())
-  { 
-     commandTupple = commandTupple + (char)Wire.read();
+  // Read next byte in
+  command[commandIndex] = Wire.read();
+  Serial.print((int)command[commandIndex]);
+  
+  // See if we got the command termination byte
+  if(command[commandIndex] == '\0'){
+    
+    if ( strncmp(SET_RES_COMMAND, command, CMD_LENGTH ) == 0 ) {
+      set_r( atoi(command + CMD_LENGTH) );
+    }
+    else if ( strncmp(SET_IND_COMMAND, command, CMD_LENGTH) == 0 ) {
+      set_l( atoi(command + CMD_LENGTH) );
+    }
+    else if ( strncmp(SET_CAP_COMMAND, command, CMD_LENGTH) == 0 ) {
+      set_c( atoi(command + CMD_LENGTH) );
+    }
+    else{
+      Serial.println("Got unrecognized command!");
+    }
+    
+    commandIndex = -1;
   }
   
-  // Inclusive starting index, Optional exclusive ending index
-  commandOperation = commandTupple.substring(0, CMD_LENGTH + 1)
-  commandValue = commandTupple.substring(CMD_LENGTH + 1);
-  
-  if ( commandOperation.equals(SET_RES_COMMAND) ) {
-    set_r( atoi(commandValue) );
-  }
-  
-  if ( commandOperation.equals(SET_IND_COMMAND) ) {
-    set_l( atoi(commandValue) );
-  }
-  
-  if ( commandOperation.equals(SET_CAP_COMMAND) ) {
-    set_c( atoi(commandValue) );
+  if(commandIndex < MAX_CMD_LENGTH){
+      commandIndex++;
   }
 }
 
@@ -142,56 +93,11 @@ void sendData()
  * Decimal values should be rouneded down. Values represents Ohms.
  */
 void set_r(int rToSet) {
-  
-  int resRemaining = rToSet;
-  
-  if (resRemaining >= 1000) {
-    digitalWrite(Res1000, HIGH);
-    resRemaining -= 1000;
-  }
-  
-  if (resRemaining >= 500) {
-    digitalWrite(Res500, HIGH);
-    resRemaining -= 500;
-  }
-  
-  if (resRemaining >= 250) {
-    digitalWrite(Res250, HIGH);
-    resRemaining -= 250;
-  }
-  
-  if (resRemaining >= 100) {
-    digitalWrite(Res100, HIGH);
-    resRemaining -= 100;
-  }
-  
-  if (resRemaining >= 50) {
-    digitalWrite(Res50, HIGH);
-    resRemaining -= 50;
-  }
-  
-  if (resRemaining >= 25) {
-    digitalWrite(Res25, HIGH);
-    resRemaining -= 25;
-  }
-  
-  if (resRemaining >= 12) {
-    digitalWrite(Res12, HIGH);
-    resRemaining -= 12;
-  }
-  
-  if (resRemaining >= 5) {
-    digitalWrite(Res5, HIGH);
-    resRemaining -= 5;
-  }
-  
-  if (resRemaining >= 2) {
-    digitalWrite(Res2_5, HIGH);
-    resRemaining -= 2;
-  }
+  Serial.print("Set resistance to ");
+  Serial.println(rToSet);
   
   // Report the remaining resistance if an exact match was not made
-  response = resRemaining;
+  response = 0;
   
 } // end SetR
 
@@ -200,56 +106,11 @@ void set_r(int rToSet) {
  * Decimal values should be rouneded down. Values represents mH.
  */
 void set_l(int lToSet) {
-  int indRemaining = lToSet;
-  
-  if (indRemaining >= 8000) {
-    digitalWrite(Ind8, HIGH);
-    indRemaining -= 8000;
-  }
-  
-  
-  if (indRemaining >= 4000) {
-    digitalWrite(Ind4, HIGH);
-    indRemaining -= 4000;
-  }
-  
-  if (indRemaining >= 2000) {
-    digitalWrite(Ind2, HIGH);
-    indRemaining -= 2000;
-  }
-  
-  if (indRemaining >= 1000) {
-    digitalWrite(Ind1, HIGH);
-    indRemaining -= 1000;
-  }
-  
-  if (indRemaining >= 500) {
-    digitalWrite(Ind500m, HIGH);
-    indRemaining -= 500;
-  }
-  
-  if (indRemaining >= 250) {
-    digitalWrite(Ind250m, HIGH);
-    indRemaining -= 250;
-  }
-  
-  if (indRemaining >= 125) {
-    digitalWrite(Ind125m, HIGH);
-    indRemaining -= 125;
-  }
-  
-  if (indRemaining >= 62) {
-    digitalWrite(Ind62_5m, HIGH);
-    indRemaining -= 62;
-  }
-  
-  if (indRemaining >= 31) {
-    digitalWrite(Ind31_25m, HIGH);
-    indRemaining -= 31;
-  }
+  Serial.print("Set inductance to ");
+  Serial.println(lToSet);
   
   // Report remaining inductance if an exact match was not made
-  response = indRemaining;
+  response = 0;
   
 } // end SetL
 
@@ -259,66 +120,11 @@ void set_l(int lToSet) {
  * Decimal values should be rouneded down. Values represents Farads.
  */
 void set_c(int cToSet) {
-  
-  capRemaining = cToSet;
-  
-  if (capRemaining >= 18800) {
-    digitalWrite(Cap18800, HIGH);
-    capRemaining -= 18800;
-  }
-  
-  if (capRemaining >= 9400) {
-    digitalWrite(Cap9400, HIGH);
-    capRemaining -= 9400;
-  }
-  
-  if (capRemaining >= 4700) {
-    digitalWrite(Cap4700, HIGH);
-    capRemaining -= 4700;
-  }
-  
-  if (capRemaining >= 2200) {
-    digitalWrite(Cap2200, HIGH);
-    capRemaining -= 2200;
-  }
-  
-  if (capRemaining >= 1000) {
-    digitalWrite(Cap1000, HIGH);
-    capRemaining -= 1000;
-  }
-  
-  if (capRemaining >= 680) {
-    digitalWrite(Cap680, HIGH);
-    capRemaining -= 680;
-  }
-  
-  if (capRemaining >= 330) {
-    digitalWrite(Cap330, HIGH);
-    capRemaining -= 330;
-  }
-  
-  if (capRemaining >= 120) {
-    digitalWrite(Cap120, HIGH);
-    capRemaining -= 120;
-  }
-  
-  if (capRemaining >= 68) {
-    digitalWrite(Cap68, HIGH);
-    capRemaining -= 68;
-  }
-  
-  if (capRemaining >= 33) {
-    digitalWrite(Cap33, HIGH);
-    capRemaining -= 33;
-  }
-  
-  if (capRemaining >= 15) {
-    digitalWrite(Cap15, HIGH);
-    capRemaining -= 15;
-  }
+  Serial.print("Set capacitance to ");
+  Serial.println(cToSet);
   
    // Report remaining capacitance if an exact match was not made
-  response = capRemaining;
+  response = 0;
   
 } // end SetC
 
